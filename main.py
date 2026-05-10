b7d980f53f8fdc1fd218665cecccf3bc93c1cf37
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-05-10 15:19:11 +0530

    added extract_data func

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ API_KEY = os.getenv("API_KEY")
 
 # openweather url
 # base_url = f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API_KEY}"
-base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+base_url = f"https://api.openweathermap.org/data/2.5/weather"
 
 print(base_url)
+
+def extract_data(city: str) -> dict:
+    url = f"{base_url}?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    # print(response.url)
+    return response.json()
+
+print(extract_data('Mumbai'), type(extract_data('Mumbai')))
+
+def transform_data(data: dict) -> dict:
+    pass
