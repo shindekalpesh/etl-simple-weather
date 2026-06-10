391733889ce78adb25f462c436ef8fa5ccd924ea
Kalpesh Shinde <kalpeshtheofficial@gmail.com>
2026-06-10 12:54:44 +0530

    added timestamp to csv file save, handled exception for no internet connection.

--- main.py
+++ main.py
@@ -3,54 +3,66 @@ import requests
 from dotenv import load_dotenv
 
 import pandas as pd
-
-# import API_KEY from .env
+from datetime import date, datetime
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-# openweather url
-# base_url = f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API_KEY}"
 base_url = f"https://api.openweathermap.org/data/2.5/weather"
-
-print(base_url)
+# print(base_url)
 
 def extract_data(city: str) -> dict:
-    print("extract_data is running.")
     url = f"{base_url}?q={city}&appid={API_KEY}"
     response = requests.get(url)
+
+    print("extract_data func has completed.")
     # print(response.json())
     return response.json()
 
 # print("extract_data('Mumbai')", type(extract_data('Mumbai')), extract_data('Mumbai'))
 
 def transform_data(data: dict) -> dict:
-    print("transform_data is running.")
+    global timestamp
+    timestamp = datetime.now()
+    # print("timestamp", type(timestamp), timestamp)
+    timestamp = timestamp.strftime("%d%m%Y_%H%M%S")
+    # print("timestamp", type(timestamp), timestamp)
+
     transformed_data = {
         "city": data['name'],
+        "timestamp": timestamp,
         "latitude": data['coord']['lat'],
         "longitude": data['coord']['lon'],
         "temp": data['main']['temp'],
         "description": data['weather'][0]['description']
     }
 
-    return transformed_data
+    print("transform_data func has completed.")
+    return transformed_data, timestamp
 
 # data = extract_data('Mumbai')
 # print("transform_data(data)", type(transform_data(data)), transform_data(data))
 
 
 def load_data(data: dict, filename: str) -> None:
-    print("load_data is running.")
     df = pd.DataFrame([data])
     df.to_csv(filename, index=False)
-
+    print("load_data func has completed.")
 # load_data(data)
 
 def run_etl_pipeline(city: str) -> None:
-    extracted_data = extract_data(city)
-    transformed_data = transform_data(extracted_data)
-    load_data(transformed_data, f'weather_data_{city}.csv')
+    
+    try:
+        extracted_data = extract_data(city)
+        transformed_data = transform_data(extracted_data)
+        load_data(transformed_data, f'weather_data_{city}{timestamp}.csv')
+    
+        print(f'File weather_data_{city}_{timestamp}.csv created.')
 
+    # except ConnectionError as e:
+    except requests.exceptions.ConnectionError as e:
+        # print(e.__str__)
+        print("No internet connection")
+    
 city = 'Mumbai'
 run_etl_pipeline(city)
